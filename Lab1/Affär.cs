00c8dc984942c3458080124cfe0a7e05cbf0601a
Hastyar <134815942+Hastyar1999@users.noreply.github.com>
2026-09-21 12:50:07 +0200

    Remove Student class from Affär.cs

--- Lab1/Affär.cs
+++ Lab1/Affär.cs
@@ -2,23 +2,3 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Lab1
-{
-    public class Student : Användare
-    {
-        public Student(
-            string användarId,
-            string förnamn,
-            string efternamn,
-            string ePost,
-            string telefonnummer)
-            : base(
-                  användarId,
-                  förnamn,
-                  efternamn,
-                  ePost,
-                  telefonnummer)
-        {
-        }
-    }
-}
