9960b95d7f68800a797c10ff5c823690dc413983
Hastyar <134815942+Hastyar1999@users.noreply.github.com>
2026-09-21 12:50:27 +0200

    Add Student class inheriting from Användare

--- Lab1/Student.cs
+++ Lab1/Student.cs
@@ -4,6 +4,21 @@ using System.Text;
 
 namespace BookSwap
 {
-    
+    public class Student : Användare
+    {
+        public Student(
+            string användarId,
+            string förnamn,
+            string efternamn,
+            string ePost,
+            string telefonnummer)
+            : base(
+                  användarId,
+                  förnamn,
+                  efternamn,
+                  ePost,
+                  telefonnummer)
+        {
+        }
+    }
 }
-
