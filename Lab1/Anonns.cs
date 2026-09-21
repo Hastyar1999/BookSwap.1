eb8cfd22b56083146021da3c198cadf4642755aa
Hastyar <134815942+Hastyar1999@users.noreply.github.com>
2026-09-21 12:51:54 +0200

    Rename class Anonns to Annons and update members

--- Lab1/Anonns.cs
+++ Lab1/Anonns.cs
@@ -4,8 +4,67 @@ using System.Text;
 
 namespace BookSwap
 {
-    public abstract class Anonns
+    public abstract class Annons
     {
-        
+        private string annonsId;
+        private string titel;
+        private double prisuppgift;
+        private DateTime publiceringsdatum;
+        private string status;
+        private string skick;
+
+     
+
+        public Annons(
+            string annonsId,
+            string titel,
+            double prisuppgift,
+            DateTime publiceringsdatum,
+            string status,
+            string skick)
+        {
+            this.annonsId = annonsId;
+            this.titel = titel;
+            this.prisuppgift = prisuppgift;
+            this.publiceringsdatum = publiceringsdatum;
+            this.status = status;
+            this.skick = skick;
+        }
+
+        public string GetAnnonsId()
+        {
+            return annonsId;
+        }
+
+        public string GetTitel()
+        {
+            return titel;
+        }
+
+        public double GetPrisuppgift()
+        {
+            return prisuppgift;
+        }
+
+        public DateTime GetPubliceringsdatum()
+        {
+            return publiceringsdatum;
+        }
+
+        public string GetStatus()
+        {
+            return status;
+        }
+
+        public void SetStatus(string status)
+        {
+            this.status = status;
+        }
+
+        public string GetSkick()
+        {
+            return skick;
+        }
+
     }
 }
