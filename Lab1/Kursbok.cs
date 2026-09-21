b2d0d21ea0256f340d92583a6b22111a90108daf
Hastyar <134815942+Hastyar1999@users.noreply.github.com>
2026-09-21 12:52:45 +0200

    Add Kursbok class with properties and constructor

--- Lab1/Kursbok.cs
+++ Lab1/Kursbok.cs
@@ -4,6 +4,49 @@ using System.Text;
 
 namespace BookSwap
 {
+    public class Kursbok : Annons
+    {
+        private string isbn;
+        private string författare;
+        private string upplaga;
 
+        public Kursbok(
+            string annonsId,
+            string titel,
+            double prisuppgift,
+            DateTime publiceringsdatum,
+            string status,
+            string skick,
+            string isbn,
+            string författare,
+            string upplaga)
+            : base(
+                  annonsId,
+                  titel,
+                  prisuppgift,
+                  publiceringsdatum,
+                  status,
+                  skick,
+                  )
+        {
+            this.isbn = isbn;
+            this.författare = författare;
+            this.upplaga = upplaga;
+        }
+
+        public string GetIsbn()
+        {
+            return isbn;
+        }
+
+        public string GetFörfattare()
+        {
+            return författare;
+        }
+
+        public string GetUpplaga()
+        {
+            return upplaga;
+        }
     }
-
+}
