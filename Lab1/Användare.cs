802234f5553968539b8de12337bbe663b0b21091
Hastyar <134815942+Hastyar1999@users.noreply.github.com>
2026-09-21 12:49:35 +0200

    Add user properties and getter methods in Användare

--- Lab1/Användare.cs
+++ Lab1/Användare.cs
@@ -6,5 +6,49 @@ namespace BookSwap
 {
     public class Användare
     {
+        private string användarId;
+        private string förnamn;
+        private string efternamn;
+        private string ePost;
+        private string telefonnummer;
+
+        public Användare(
+            string användarId,
+            string förnamn,
+            string efternamn,
+            string ePost,
+            string telefonnummer)
+        {
+            this.användarId = användarId;
+            this.förnamn = förnamn;
+            this.efternamn = efternamn;
+            this.ePost = ePost;
+            this.telefonnummer = telefonnummer;
+        }
+
+        public string GetAnvändarId()
+        {
+            return användarId;
+        }
+
+        public string GetFörnamn()
+        {
+            return förnamn;
+        }
+
+        public string GetEfternamn()
+        {
+            return efternamn;
+        }
+
+        public string GetEPost()
+        {
+            return ePost;
+        }
+
+        public string GetTelefonnummer()
+        {
+            return telefonnummer;
+        }
     }
 }
